41b100228533c9c365e39672297e67872f1d4ec5
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-27 21:59:54 +0200

    css and js

--- main.js
+++ main.js
@@ -6,18 +6,29 @@ let divY = 50;
 div.style.left = `${divX}px`;
 div.style.top = `${divY}px`;
 
+let drawActive = false;
 
-div.addEventListener('mousedown', () => {
+let insertDivX;
+let insertDivY;
+
+div.addEventListener('mousedown', (e) => {
     div.style.background = 'gray';
+    drawActive = !drawActive;
+
+    insertDivX = e.offsetX;
+    insertDivY = e.offsetY;
 })
 
 div.addEventListener('mousemove', (e) => {
-    divx = e.clientX;
-    divY = e.clientY;
-    div.style.left = `${divX}px`;
-    div.style.top = `${divY}px`;
+    if(drawActive){
+        divX = e.clientX;
+        divY = e.clientY;
+        div.style.left = `${divX - 50}px`;
+        div.style.top = `${divY - 50}px`;
+    }
 })
 
 div. addEventListener('mouseup', () => {
-    div.style.background = 'black'
+    div.style.background = 'black';
+    drawActive = !drawActive;
 })
